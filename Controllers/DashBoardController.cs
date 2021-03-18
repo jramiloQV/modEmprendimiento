fe8697b4d1db818ceea516b99ad3cb4a3a8b1f80
Andres Cabezas <acabezas@qvision.com.co>
2021-03-18 16:16:48 -0500

    Grafico

---
+++ Controllers/DashBoardController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace modEmprendimiento.Controllers
+{
+    public class DashBoardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
